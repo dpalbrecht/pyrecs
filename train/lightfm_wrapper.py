75e65e1f0cab4d4ef8cb5a3f948955c569f59f99
David Albrecht <davidpabloalbrecht@gmail.com>
2022-03-05 13:02:08 -0800

    start lightfm wrapper and tests

---
+++ train/lightfm_wrapper.py
@@ -0,0 +1,56 @@
+import lightfm
+from scipy.sparse import coo_matrix
+import numpy as np
+from ..evaluate import mapk
+
+class LightFM:
+    def __init__(self, rows_col, columns_col, interactions_type):
+        self.rows_col = rows_col
+        self.columns_col = columns_col
+        self.interactions_type = interactions_type
+        
+    def _make_interactions_matrix(self, flat_interactions_df):
+        self.rows2ind = dict(zip(flat_interactions_df[self.rows_col].unique(),
+                                 range(flat_interactions_df[self.rows_col].nunique())))
+        self.columns2ind = dict(zip(flat_interactions_df[self.columns_col].unique(),
+                                    range(flat_interactions_df[self.columns_col].nunique())))
+        if self.interactions_type == 'ones':
+            flat_interactions_df.drop_duplicates(subset=[self.rows_col, self.columns_col], inplace=True)
+            rows = flat_interactions_df[self.rows_col].map(self.rows2ind).values
+            cols = flat_interactions_df[self.columns_col].map(self.columns2ind).values
+            interactions = np.ones(len(flat_interactions_df))
+        elif self.interactions_type == 'counts':
+            counts_dict = dict(flat_interactions_df.groupby([self.rows_col, self.columns_col]).size())
+            unique_row_column_pairs = flat_interactions_df.groupby('users')['items'].unique().reset_index().explode('items').values
+            rows, cols, interactions = [], [], []
+            for unique_pair in unique_row_column_pairs:
+                row, column = unique_pair
+                rows.append(self.rows2ind[row])
+                cols.append(self.columns2ind[column])
+                interactions.append(counts_dict[(row, column)])
+        else:
+            raise ValueError(f"interactions_type ('{self.interactions_type}') not implemented.")
+        self.interactions_matrix = coo_matrix((interactions, (rows, cols)), 
+                                              shape=(len(self.rows2ind), len(self.columns2ind)))
+    
+    def preprocess(self, flat_interactions_df):
+        self._make_interactions_matrix(flat_interactions_df)
+        
+    def evaluate(self):
+        # TODO: 
+        pass
+    
+    def train(self, lightfm_model, train_kwargs):
+        ones_interactions_matrix = deepcopy(interactions_matrix)
+        ones_interactions_matrix = ones_interactions_matrix[ones_interactions_matrix >= 1] = 1
+        for i in range(train_kwargs['num_epochs']):
+            lightfm.fit_partial(interactions=ones_interactions_matrix, 
+                                user_features=train_kwargs['user_features'], 
+                                item_features=train_kwargs['item_features'], 
+                                sample_weight=self.interactions_matrix, 
+                                epochs=1, num_threads=train_kwargs['num_threads'], 
+                                verbose=False)
+    
+    def run(self, flat_interactions_df, lightfm_model, train_kwargs):
+        self.preprocess(flat_interactions_df)
+        self.train(lightfm_model, train_kwargs)
---
+++ train/tests/test_lightfm_wrapper.py
@@ -0,0 +1,74 @@
+from .. import lightfm_wrapper
+import pytest
+import pandas as pd
+import numpy as np
+
+
+def create_df_2users_3items_yes_duplicates(duplicates=True):
+    df = pd.DataFrame({'users':['user1','user1','user1','user2'], 
+                       'items':['item1','item1','item3','item2']})
+    if not duplicates:
+        return df.drop_duplicates()
+    return df
+
+def create_df_3users_2items_yes_duplicates(duplicates=True):
+    df = pd.DataFrame({'users':['user1','user3','user1','user2'], 
+                       'items':['item1','item1','item1','item2']})
+    if not duplicates:
+        return df.drop_duplicates()
+    return df
+
+def create_df_3users_3items_no_duplicates(duplicates=True):
+    df = pd.DataFrame({'users':['user1','user2','user3','user3'], 
+                       'items':['item1','item2','item3','item1']})
+    if not duplicates:
+        return df.drop_duplicates()
+    return df
+
+def create_df_3users_3items_yes_duplicates(duplicates=True):
+    df = pd.DataFrame({'users':['user1','user2','user3','user3'], 
+                       'items':['item1','item2','item3','item3']})
+    if not duplicates:
+        return df.drop_duplicates()
+    return df
+
+def format_test_parameters(funcs):
+    parameters = []
+    for func in funcs:
+        parameters.extend([
+            (func(), 'ones'),
+            (func(), 'counts'),
+            (func(duplicates=True), 'ones'),
+            (func(duplicates=True), 'counts')
+        ])
+    return parameters
+
+@pytest.mark.parametrize("df,interactions_type",
+                         format_test_parameters([create_df_2users_3items_yes_duplicates, 
+                                                 create_df_3users_2items_yes_duplicates, 
+                                                 create_df_3users_3items_no_duplicates, 
+                                                 create_df_3users_3items_yes_duplicates])
+                        )
+def test_make_interactions(df, interactions_type):
+    lfm = lightfm_wrapper.LightFM(rows_col='users', columns_col='items', interactions_type=interactions_type)
+    lfm._make_interactions_matrix(df.copy())
+    interactions_matrix = lfm.interactions_matrix.todense()
+    if interactions_type == 'ones':
+        df.drop_duplicates(inplace=True)
+        
+    # Check that rows have the right number of interactions
+    row_counts = df.groupby('users').size().reset_index().rename(columns={0:'count'})
+    row_counts['user_inds'] = row_counts['users'].map(lfm.rows2ind)
+    rowind2count = dict(zip(row_counts['user_inds'], row_counts['count']))
+    for row, count in rowind2count.items():
+        assert interactions_matrix[row].sum() == count
+        
+    # Check that columns have the right number of interactions
+    column_counts = df.groupby('items').size().reset_index().rename(columns={0:'count'})
+    column_counts['item_inds'] = column_counts['items'].map(lfm.columns2ind)
+    columnind2count = dict(zip(column_counts['item_inds'], column_counts['count']))
+    for column, count in columnind2count.items():
+        assert interactions_matrix[:,column].sum() == count
+        
+    # Check that there are the right number of dimensions
+    assert interactions_matrix.shape == (len(rowind2count), len(columnind2count))
