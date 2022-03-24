3660c957f2d402e7a60f18f71f6e02be42046b16
David Albrecht <davidpabloalbrecht@gmail.com>
2022-03-23 20:44:24 -0700

    start new batch streaming process

--- predict/tests/test_tfrs_streaming.py
+++ predict/tests/test_tfrs_streaming.py
@@ -43,3 +43,26 @@ def test_predictions(inputs_dict, expected_predictions):
                                          n_recs=inputs_dict['n_recs'],
                                          user2excludeitems=inputs_dict['user2excludeitems'])
     assert predictions == expected_predictions
+    
+
+# @pytest.mark.parametrize("inputs_dict,expected_predictions",
+#                          [
+#                              ({'user_identifiers':np.array(['user1', 'user2']), 
+#                                'user_embeddings':np.array([[1,2],[1,5]]), 
+#                                'item_identifiers':np.array(['a','b','c']), 
+#                                'item_embeddings':np.array([[1,2],[1,2],[1,2]]),
+#                                'train_user_interactions':{},
+#                                'popular_items':[],
+#                                'n_recs':10},
+#                               {'user1': ['a', 'b', 'c'], 'user2': ['a', 'b', 'c']})
+#                          ]
+#                         )
+# def test_predictions2(inputs_dict, expected_predictions):
+#     predictions = tfrs_streaming.predict2(user_identifiers=inputs_dict['user_identifiers'], 
+#                                          user_embeddings=inputs_dict['user_embeddings'],
+#                                          item_identifiers=inputs_dict['item_identifiers'], 
+#                                          item_embeddings=inputs_dict['item_embeddings'],
+#                                          n_recs=inputs_dict['n_recs'],
+#                                          train_user_interactions=inputs_dict['train_user_interactions'],
+#                                          popular_items=inputs_dict['popular_items'])
+#     assert predictions == expected_predictions
--- predict/tfrs_streaming.py
+++ predict/tfrs_streaming.py
@@ -1,6 +1,7 @@
 import tensorflow_recommenders  as tfrs
 import tensorflow as tf
 import itertools
+import copy
     
 
 def predict(user_identifiers, user_embeddings,
@@ -50,5 +51,56 @@ def predict(user_identifiers, user_embeddings,
     for inputs, candidates in predictions:
         formatted_predictions.update(dict(zip(inputs.numpy().astype(str).tolist(), 
                                               candidates.numpy().astype(str).tolist())))
+        
+    return formatted_predictions
+
+
+def predict2(user_identifiers, user_embeddings,
+             item_identifiers, item_embeddings,
+             train_user_interactions={},
+             popular_items=[],
+             embedding_dtype='float32', 
+             n_recs=10,
+             prediction_batch_size=32):
+    
+    # Create tfrs.Streaming instance
+    stream = tfrs.layers.factorized_top_k.Streaming()
+    stream.index_from_dataset(
+    tf.data.Dataset.from_tensors({
+            'identifier':item_identifiers,
+            'embedding':item_embeddings.astype(embedding_dtype)
+        }).map(lambda x: (x['identifier'], x['embedding']))
+    )
+    
+    # Get batched predictions
+    if len(user_embeddings.shape) == 1:
+        user_embeddings = user_embeddings[None,:]
+        
+    batch_ranges = range(0, len(user_embeddings), prediction_batch_size)
+    user_chunks = ([user_identifiers[i:i + prediction_batch_size],
+                    user_embeddings[i:i + prediction_batch_size].astype(embedding_dtype)] 
+                   for i in batch_ranges)
+    
+    train_predictions = {}
+    max_exclude_len = max([len(v) for v in train_user_interactions.values()])
+    for user_ids, user_embs in user_chunks:
+        scores, candidate_ids_list = stream(user_embs, k=n_recs+max_exclude_len)
+        for user_id, candidate_ids in zip(user_ids, candidate_ids_list):
+            temp_popular_items = copy(popular_items)
+            temp_exclusions = train_user_interactions.get(user_id)
+            temp_candidates = []
+            for cid in candidate_ids:
+                if cid not in temp_exclusions:
+                    temp_candidates.append(cid)
+                if len(temp_candidates) == n_recs:
+                    break
+            if (len(temp_candidates) != n_recs) & (len(temp_popular_items) > 0): 
+                for pop_item in temp_popular_items:
+                    if (pop_item not in temp_exclusions) & (pop_item not in temp_candidates):
+                        temp_candidates.append(pop_item)
+                    if len(temp_candidates) == n_recs:
+                        break
+                
+            train_predictions[user_id] = temp_candidates
         
     return formatted_predictions
